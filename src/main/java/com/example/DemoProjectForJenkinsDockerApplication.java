ebb1508480901fcc5d46d18c19bb9b50b65d2ff3
AdityaGitHub-cyber <adityaspatil98@gmail.com>
2024-07-28 22:27:41 +0530

    logger addition for jenkins

--- src/main/java/com/example/DemoProjectForJenkinsDockerApplication.java
+++ src/main/java/com/example/DemoProjectForJenkinsDockerApplication.java
@@ -1,12 +1,23 @@
 package com.example;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoProjectForJenkinsDockerApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(DemoProjectForJenkinsDockerApplication.class);
+
+	@PostConstruct
+	public void init(){
+		logger.info("Application started...");
+	}
+
 	public static void main(String[] args) {
+		logger.info("Application executed...");
 		SpringApplication.run(DemoProjectForJenkinsDockerApplication.class, args);
 	}
 
